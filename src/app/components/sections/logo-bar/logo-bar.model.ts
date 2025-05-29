076e3273c6f43ed4566fa78a7a9764e54f6a19b1
farshadmrd <fa.moradi2000@gmail.com>
2025-05-29 21:37:02 +0200

    logo bar added

--- src/app/components/sections/logo-bar/logo-bar.model.ts
+++ src/app/components/sections/logo-bar/logo-bar.model.ts
@@ -4,5 +4,8 @@
 }
 
 export const Logos: Logo[] = [
-  { url: 'assets/logo/logo_bar/PhotoStudio.png', alt: 'PhotoStudio' },
+  { url: 'assets/logo/logo_bar/logo1.png', alt: 'logo1' },
+  { url: 'assets/logo/logo_bar/logo2.png', alt: 'logo2' },
+  { url: 'assets/logo/logo_bar/logo3.png', alt: 'logo3' },
+  { url: 'assets/logo/logo_bar/logo4.png', alt: 'logo4' },
 ];
