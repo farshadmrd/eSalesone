17498a221f520467dc4cd0e8cb97f92be1ecfa97
farshadmrd <fa.moradi2000@gmail.com>
2025-05-30 16:12:58 +0200

    Fix minor routing bug between landing page and checkout page

--- src/app/components/sections/header/header.component.ts
+++ src/app/components/sections/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component,input,inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { TriggerAnimationService } from "../../../services/trigger-animation.service";
 import { ScrollToService } from "../../../services/scroll-to.services";
 
@@ -13,24 +13,37 @@ export class HeaderComponent {
   TriggerAnimationService=inject(TriggerAnimationService); //trigger animation of about me
   scrollToService = inject(ScrollToService); // scroll to service
   router = inject(Router);
+  route = inject(ActivatedRoute);
   
   handleClick(){
     // Check if we're on home page for scrolling, otherwise navigate
-    if (this.router.url === '/home') {
+    if (this.router.url === '/home' || this.router.url === '/') {
       this.scrollToService.scrollTo('page2');
       this.TriggerAnimationService.triggerAnimation(); //trigger animation of about me
     } else {
-      this.router.navigate(['/home'], { fragment: 'page2' });
+      // Navigate to home and then scroll after navigation completes
+      this.router.navigate(['/home']).then(() => {
+        // Use setTimeout to ensure the DOM is updated after navigation
+        setTimeout(() => {
+          this.scrollToService.scrollTo('page2');
+          this.TriggerAnimationService.triggerAnimation();
+        }, 100);
+      });
     }
   }
 
   handleServicesClick(sectionId: string){
     // Navigate to services or checkout pages
     if (sectionId === 'page3') {
-      if (this.router.url === '/home') {
+      if (this.router.url === '/home' || this.router.url === '/') {
         this.scrollToService.scrollTo(sectionId);
       } else {
-        this.router.navigate(['/home'], { fragment: 'page3' });
+        // Navigate to home and then scroll after navigation completes
+        this.router.navigate(['/home']).then(() => {
+          setTimeout(() => {
+            this.scrollToService.scrollTo(sectionId);
+          }, 100);
+        });
       }
     } else if (sectionId === 'checkout') {
       this.router.navigate(['/checkout']);
